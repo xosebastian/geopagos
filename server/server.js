37d6b3dadc15112ad4f9f273686baff6af70a6d0
Sebastian <sebastian_cardozo@itrsa.com.ar>
2018-05-10 00:05:24 -0300

    First commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+ 
+app.get('/', function (req, res) {
+  res.json('Hello World')
+})
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+/* ---------------------- */
+
+/* Create User */
+app.post('/user/create', (req, res) => {
+    res.json('Se creo el usuario correctamente')
+})
+
+/* Edit User */
+app.put('/user/edit', (req, res) => {
+    res.json('Editar Usuario')
+})
+
+/* Delete User */
+app.delete('/user/delete/:email', (req, res) => {
+    let email = req.params.email;
+    res.json(`Eliminar usuario: ${email}`)
+})
+
+
+/* Validar Usuario */
+app.put('/user/validate', (req, res) => {
+    res.json('Se creo el usuario correctamente')
+})
+
+
+ 
+app.listen(3000, () => {
+    console.log('Escuchando el puerto:', 3000);
+})
